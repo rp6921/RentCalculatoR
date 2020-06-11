802635e0c45f177c6911e251c85d1163d37ba76a
rp6921 <61269963+rp6921@users.noreply.github.com>
2020-06-11 10:11:39 +0200

    Update RentCalculatoR.R

--- R/RentCalculatoR.R
+++ R/RentCalculatoR.R
@@ -1,4 +1,4 @@
-#' @title Changes in rents calculator for different situations (according to Swiss law))
+#' @title Changes in rents calculator for different situations (according to Swiss law)
 #'
 #' @description With the package RentCalculatoR you can calculate the change in rent of residential real estate according to Swiss law. Changes in value-adding investments, depreciation, interests and reference interest rates can be taken into account. As well as changings due to changes in the market such as inflation, mortgage and cost increments.
 #'
